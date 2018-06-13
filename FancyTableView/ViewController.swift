2e5ca9b682bf528e4e998c2ef482b74e4628a271
Tristan Wolf <wolftristan70@gmail.com>
2018-06-13 13:25:54 -0400

    Initial Commit

---
+++ FancyTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FancyTableView
+//
+//  Created by Tristan Wolf on 2018-06-13.
+//  Copyright © 2018 Tristan Wolf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
